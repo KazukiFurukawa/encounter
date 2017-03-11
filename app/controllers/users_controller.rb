be7e65132505a0024871974f430c95571de5dde2
かずき <kazuki.furukawa3@gmail.com>
2017-03-11 19:37:25 +0900

    アソシエーションの実装、ツイートのタイムラインにユーザー名の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user_name = current_user.user_name
-    @posts = Post.where(user_id: current_user.id).order("created_at DESC")
+    @posts = current_user.posts.order("created_at DESC")
   end
 
 end
